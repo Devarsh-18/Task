48a869bb23e9d9de6cc4cb4d4911342a87fb0ef9
devarsh.shukla <devarshshukla64@gmail.com>
2025-05-26 11:54:53 +0530

    merge others

--- task10_triangle.java
+++ task10_triangle.java
@@ -1,19 +1,16 @@
 package test_script.test_script;
 
-import java.util.Scanner;
+public class task10_1_triangle {
+	public static void main(String[] args) {
+		int a = 5;
+		int b =5;
+		int c = 5;
+		if (a == b && b == c) {
+            System.out.println("The triangle is equilateral.");
+        } else if (a == b || b == c || a == c) {
+            System.out.println("The triangle is isosceles.");
+        } else {
+            System.out.println("The triangle is scalene.");   }}}
+	
 
-public class task10_triangle {
-public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);  
-        System.out.print("Enter the length of side a: ");
-        double a = scanner.nextDouble();
-        System.out.print("Enter the length of side b: ");
-        double b = scanner.nextDouble();
-        System.out.print("Enter the length of side c: ");
-        double c = scanner.nextDouble();
-        if(a ==b && b==c) {
-        	System.out.println("equilatral");}
-        	else if(a==b || b==c || a==c) {
-        		System.out.println("isosceles");}
-        	else {
-        		System.out.println("scalable");    }}}
+
